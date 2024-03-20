aae41cc9eb1c56b24feaa33c687915407fcc17a8
JDDPacheco <josediogo.dp@gmail.com>
2024-03-20 18:59:57 -0400

    Commit inicial da Aula do dia 20/03/2024

---
+++ aula_20-03-2024.sql
@@ -0,0 +1,13 @@
+use bd2024;
+
+/*
+Aula do dia 20/03/2024
+*/
+
+/*
+Criar a tabela funcionariocomissao(fcfuncodigo,fcanomes,fcvalor) relacionada a tabela funcionario.
+fcanomes: char(6) - ex.: "202403"
+fcvalor: decimal (6,2) - ex.: 9999,99
+
+fórmula do valor da comissão: valcomissao = 
+*/
