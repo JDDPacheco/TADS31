5e5cfa5c09cee98b6f63a0d0458156c416cc6275
JDDPacheco <josediogo.dp@gmail.com>
2024-04-02 21:34:56 -0400

    Inicio do exercicio de banco

---
+++ Aula_02-04-2024/banco_de_dados.sql
@@ -0,0 +1,67 @@
+use clinica_medica:
+
+create table if not exists clinica_medica.funcionarios(
+	Codigo_Funcionario int not null auto_increment,
+  Nome_Completo varchar(50) null,
+  Numero_RG varchar(12) null,
+  Orgao_Emissor varchar(6) null,
+  Numero_CPF varchar(14) null,
+  Endereco varchar(50) null,
+  Numero varchar(15) null,
+  Complemento varchar(30) null,
+  Bairro varchar(40) null,
+  Cidade varchar(40) null,
+  Estado varchar(2) null,
+  Telefone varchar(20) null,
+  Celular varchar(20) null,
+  Numero_CTPS varchar(20) null,
+  Numero_PIS varchar(20) null,
+  Data_Nascimento date null,
+  primary key (Codigo_Funcionario),
+  index idx_Nome (Nome_Completo asc),
+  index idx_RG (Numero_RG asc),
+  index idx_CPF (Numero_CPF asc))
+ENGINE = InnoDB;
+
+create table if not exists clinica_medica.usuarios(
+  Registro_Usuario int not null auto_increment,
+  Identificacao_Usuario varchar(20) null,
+  Senha_Acesso varchar(10) null,
+  Cadastro_Funcionario varchar(1) null default 'N',
+  Cadastro_Usuario varchar(1) null default 'N',
+  Cadastro_Paciente varchar(1) null default 'N',
+  Cadastro_Especialidade varchar(1) null default 'N',
+  Cadastro_Medico varchar(1) null default 'N',
+  Cadastro_Convenio varchar(1) null default 'N',
+  Agendamento_Consulta varchar(1) null default 'N',
+  Cancelamento_Consulta varchar(1) null default 'N',
+  Modulo_Adiministrativo varchar(1) null default 'N',
+  Modulo_Agendamento varchar(1) null default 'N',
+  Modulo_Atendimento varchar(1) null default 'N',
+  primary key (Registro_Usuario))
+ENGINE = InnoDB;
+
+create table if not exists clinica_medica.especialidades (
+    Codigo_Especialidades int not null auto_increment,
+    Descricao_Especialidade varchar(45) null,
+    primary key (Codigo_Especialidades))
+ENGINE = InnoDB;
+
+create table if not exists clinica_medica.medicos (
+    Codigo_Medico int not null auto_increment,
+    Nome_Medico varchar(50) null,
+    Codigo_Especialidade int not null,
+    CRM varchar(20) null,
+    primary key (Codigo_Medico, Codigo_Especialidade),
+    index fk_medicos_especialidade1_idx (Codigo_Especialidade asc),
+        foreing key (Codigo_Especialidade)
+        references clinica_medica.especialidades (Codigo_Especialidade)
+        on delete no action
+        on update no action)
+ENGINE = InnoDB;
+
+create table if not exists clinica_medica.convenios(
+    Codigo_Convenio int not null auto_increment,
+    Empresa_Convenio varchar(45) null,
+    
+)
