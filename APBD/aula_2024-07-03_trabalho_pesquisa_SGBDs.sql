a97fc45e0efda97d628b55736c0a66e312881a3b
jddpacheco <josediogo.dp@gmail.com>
2024-07-03 18:53:06 -0400

    novo trabalho em grupo

---
+++ APBD/aula_2024-07-03_trabalho_pesquisa_SGBDs.sql
@@ -0,0 +1,16 @@
+-- aula dia 03/07/2024
+
+/*
+Trabalho em grupo, 4 membros.
+Conhecer os outros SGBDs:
+	1. SQL Server
+    2. Oracle
+    3. PostgreSQL
+    4. BD2
+    5. SyBase
+    6. Informix
+    
+    1ª parte: Fazer uma tabela comparativa entre os SGBDs contemplando sintaxe SQL, requisitos de hardware, suporte a programação (SP, Functions, Triggers, Events, outros), Sistemas Operacionais, funcionalidades avançadas (SQL) e tecnologias adicionais (SQL).
+    
+    Link do trabalho: https://docs.google.com/spreadsheets/d/1Rzo8wbJM9VhgOWFN7id0kirkiQ-FGpXuWczMBFygtSY/
+*/
