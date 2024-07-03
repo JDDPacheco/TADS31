2478e1bda41cd011aabe7d5bd1e6375e9a1e87c6
jddpacheco <josediogo.dp@gmail.com>
2024-07-03 19:51:17 -0400

    atualizando

--- APBD/aula_2024-07-03_trabalho_pesquisa_SGBDs.sql
+++ APBD/aula_2024-07-03_trabalho_pesquisa_SGBDs.sql
@@ -14,3 +14,24 @@ Conhecer os outros SGBDs:
     
     Link do trabalho: https://docs.google.com/spreadsheets/d/1Rzo8wbJM9VhgOWFN7id0kirkiQ-FGpXuWczMBFygtSY/
 */
+
+use bd2024;
+
+drop procedure if exists sp_lista_cliente_sexo_bairro;
+
+delimiter ##
+create procedure sp_lista_cliente_sexo_bairro(psexo char(1),
+												pbairro varchar(40))
+begin
+    select clicodigo Codigo, clinome CLiente,
+			clisexo Sexo, clirendamensal Renda,
+            estdescricao Estado_Civil, bainome Bairro
+	from cliente
+    inner join bairro on baicodigo = clibaicodigo
+    inner join estadocivil on estcodigo = cliestcodigo
+    where bainome = pbairro and clisexo = psexo
+    order by clinome;
+end ##
+delimiter ;
+
+call sp_lista_cliente_sexo_bairro('f', 'cachoeirinha');
